5a270362ba7c3b7ade902eb1cb9fbbd622addd49
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2021-11-10 18:29:22 +0000

    Offer more search options
    - case sensitivity
    - search in title, description or content
    - remove whole word search

--- main.js
+++ main.js
@@ -10,11 +10,18 @@ const state = {
   selectedWords: '',
   excludedWords: '',
   contentIds: '',
+  searchQuery: null,
   searchResults: null,
   showFields: {
     contentId: true,
     documentType: true
   },
+  whereToSearch: {
+    title: true,
+    description: false,
+    text: false
+  },
+  caseSensitive: false,
   activeMode: 'keyword-search', // or 'contentid-search'
   waiting: false // whether we're waiting for a request to return
 };
@@ -26,7 +33,7 @@ const contentIdSearchButtonClicked = function() {
     .filter(d=>d.length>0)
     .map(s => s.toLowerCase());
   const whereStatement = contentIds.map(cid => `n.contentID="${cid}" `).join(' OR ');
-  const query = `match (n:Cid) where ${whereStatement} return
+  state.searchQuery = `match (n:Cid) where ${whereStatement} return
     "https://www.gov.uk"+n.name AS url,
     n.name AS slug,
     n.title,
@@ -35,8 +42,7 @@ const contentIdSearchButtonClicked = function() {
     n.publishing_app,
     n.first_published_at AS first_published_at,
     n.public_updated_at AS last_updated`;
-  console.log(query);
-  state.neo4jSession.run(query)
+  state.neo4jSession.run(state.searchQuery)
     .then(async results => {
       await handleEvent({type:'neo4j-callback-ok', data: results})
     });
@@ -51,16 +57,15 @@ const searchButtonClicked = function() {
 
   const keywords = state.selectedWords
     .split(/[,;\s]+/)
-    .filter(d=>d.length>0)
-    .map(s => s.toLowerCase());
+    .filter(d=>d.length>0);
 
   const excludedKeywords = state.excludedWords
     .split(/[,;\s]+/)
     .filter(d=>d.length>0)
     .map(s => s.toLowerCase());
 
-  const query = buildQuery(keywords, excludedKeywords, state.combinator);
-  state.neo4jSession.run(query)
+  state.searchQuery = buildQuery(state.whereToSearch, keywords, excludedKeywords, state.combinator, state.caseSensitive);
+  state.neo4jSession.run(state.searchQuery)
     .then(async results => {
       await handleEvent({type:'neo4j-callback-ok', data: results})
     });
@@ -75,6 +80,10 @@ const handleEvent = async function(event) {
         state.selectedWords = id('keyword').value;
         state.excludedWords = id('excluded-keyword').value;
         state.combinator = id('and-or').selectedIndex == 0 ? 'AND' : 'OR';
+        state.whereToSearch.title = id('search-title').checked;
+        state.whereToSearch.description = id('search-description').checked;
+        state.whereToSearch.text = id('search-text').checked;
+        state.caseSensitive = id('case-sensitive').checked;
         state.waiting = true;
         searchButtonClicked();
         break;
@@ -132,17 +141,50 @@ const view = function() {
     html.push(`
           <div class="govuk-form-group" id="keyword-search-panel">
             <p class="govuk-body">
-             Type keywords to find pages with title containing<br/>
+             Type keywords to find pages with title or content containing<br/>
               <select class="govuk-select" id="and-or">
                 <option name="and" ${state.combinator === 'and' ? 'selected' : ''}>all the words</option>
                 <option name="or" ${state.combinator === 'or' ? 'selected' : ''}>any of the words</option>
               </select>
-              <input class="govuk-input govuk-input--width-10" id="keyword" value="${state.selectedWords}"/>
+              <input class="govuk-input govuk-input--width-10" id="keyword" placeholder="eg: cat, dog, ferret" value="${state.selectedWords}"/>
 
             <br/>but not:
 
               <input class="govuk-input govuk-input--width-10" id="excluded-keyword" placeholder="leave blank if no exclusions" value="${state.excludedWords}"/>
+            </p>
+            <div id="search-locations-wrapper">
+              Search in:
+              <ul class="kg-checkboxes" id="search-locations">
+                <li class="kg-checkboxes__item">
+                  <input class="kg-checkboxes__input"
+                         type="checkbox" id="search-title"
+                         ${state.whereToSearch.title ? 'checked' : ''}/>
+                  <label class="kg-label kg-checkboxes__label">Title</label>
+                </li>
+                <li class="kg-checkboxes__item">
+                  <input class="kg-checkboxes__input"
+                         type="checkbox" id="search-description"
+                         ${state.whereToSearch.description ? 'checked' : ''}/>
+                  <label class="kg-label kg-checkboxes__label">Description</label>
+                </li>
+                <li class="kg-checkboxes__item">
+                  <input class="kg-checkboxes__input"
+                         type="checkbox" id="search-text"
+                         ${state.whereToSearch.text ? 'checked' : ''}/>
+                  <label class="kg-label kg-checkboxes__label">Text</label>
+                </li>
+              </ul>
+            </div>
 
+            <div class="kg-checkboxes">
+              <div class="kg-checkboxes__item">
+                <input class="kg-checkboxes__input"
+                       type="checkbox" id="case-sensitive"
+                       ${state.caseSensitive ? 'checked' : ''}/>
+                <label class="kg-label kg-checkboxes__label">case sensitive</label>
+              </div>
+            </div>
+            <p class="govuk-body">
               <button
                   class="govuk-button ${state.waiting?'govuk-button--secondary':''}"
                   id="keyword-search">
@@ -180,7 +222,7 @@ const view = function() {
 
 
   // adding onclick doesn't work
-  document.querySelectorAll('button, input[type=checkbox]')
+  document.querySelectorAll('button, input[type=checkbox][data-interactive=true]')
     .forEach(input => input.addEventListener(
       'click',
       event => handleEvent({type: 'dom', id: event.target.getAttribute('id')})));
@@ -191,11 +233,37 @@ const view = function() {
 
 const id = x => document.getElementById(x);
 
-const buildQuery = function(keywords, exclusions, operator) {
+const containsClause = function(field, word, caseSensitive) {
+  return caseSensitive ?
+    `(n.${field} CONTAINS "${word}")`
+  :
+    `(toLower(n.${field}) CONTAINS toLower("${word}"))`
+  ;
+
+}
+
+const multiContainsClause = function(fields, word, caseSensitive) {
+  return '(' + fields
+    .map(field => containsClause(field, word, caseSensitive))
+    .join(' OR ') + ')'
+}
+
+const buildQuery = function(fields, keywords, exclusions, operator, caseSensitive) {
+  const fieldsToSearch = [
+    fields.title?'title':null,
+    fields.description?'description':null,
+    fields.text?'text':null
+  ].filter(item => item)
+
+  console.log(caseSensitive)
+
   const inclusionClause = 'WHERE\n' +
-    keywords.map(word => `n.title =~ '(?i).*\\\\b${word}\\\\b.*'`).join(`\n ${operator} `);
+    keywords
+//      .map(word => `((n.title =~ '(?i).*\\\\b${word}\\\\b.*') OR (n.description =~ '(?i).*\\\\b${word}\\\\b.*') OR (n.text =~ '(?i).*\\\\b${word}\\\\b.*'))`)
+      .map(word => multiContainsClause(fieldsToSearch, word, caseSensitive))
+      .join(`\n ${operator} `);
   const exclusionClause = exclusions.length ?
-    ('WITH * WHERE NOT ' + exclusions.map(word => `n.title =~ '(?i).*\\\\b${word}\\\\b.*'`).join(`\n $OR `)) : '';
+    ('WITH * WHERE NOT ' + exclusions.map(word => multiContainsClause(fieldsToSearch, word, caseSensitive)).join(`\n OR `)) : '';
 
   return `MATCH
 (n:Cid)-[r:HAS_PRIMARY_PUBLISHING_ORGANISATION]->(o:Organisation)
@@ -232,12 +300,14 @@ const viewSearchResults = function(results) {
         <ul class="kg-checkboxes" id="show-fields" onclick="handleEvent">
           <li class="kg-checkboxes__item">
             <input class="kg-checkboxes__input"
+                   data-interactive="true"
                    type="checkbox" id="show-contentid"
                    ${state.showFields.contentId ? 'checked' : ''}/>
             <label class="kg-label kg-checkboxes__label">content ID</label>
           </li>
           <li class="kg-checkboxes__item">
             <input class="kg-checkboxes__input"
+                   data-interactive="true"
                    type="checkbox" id="show-doctype"
                    ${state.showFields.documentType ? 'checked' : ''}/>
             <label class="kg-label kg-checkboxes__label">Document type</label>
@@ -264,6 +334,11 @@ const viewSearchResults = function(results) {
     });
 
     html.push('</tbody></table>');
+  }
+
+  if (state.searchQuery) {
+    html.push('<h2 class="govuk-heading-s">Cypher query</h2>');
+    html.push(`<pre>${state.searchQuery}</pre>`);
   }
 
   return html.join('');
@@ -294,32 +369,3 @@ const init = async function() {
   await init();
   view();
 })();
-
-
-/*
-
-      <details class="govuk-details" data-module="govuk-details">
-        <summary class="govuk-details__summary">
-          <span class="govuk-details__summary-text">Explore</span>
-        </summary>
-        <div class="govuk-details__text">
-          <svg id=graph width=500 height=500></svg>
-        </div>
-      </details>
-
-      <details class="govuk-details" data-module="govuk-details">
-        <summary class="govuk-details__summary">
-          <span class="govuk-details__summary-text">Settings</span>
-        </summary>
-        <div class="govuk-details__text">
-          <p class="govuk-body">
-            <span>Server URI:</span>
-            <input class="govuk-input" id="uri"/>
-          </p>
-          <p class="govuk-body">user: <input class="govuk-input" id="user"/></p>
-          <p class="govuk-body">password: <input class="govuk-input" id="password" type="password"/></p>
-          <button class="govuk-button" id="connect-button">Connect</button>
-          </div>
-        </div>
-      </details>
-*/
