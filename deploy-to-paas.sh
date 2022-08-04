6f35349556608f4c1ce71a435fcc974c75fc532a
Max Froumentin <github@e97.me>
2022-08-04 11:51:50 +0100

    Re-enable GA until GTM is set up

--- deploy-to-paas.sh
+++ deploy-to-paas.sh
@@ -15,18 +15,47 @@ else
     fi
 fi
 
+if [ "${GA_ID}" != "" ]; then
+    echo "GA_ID is set to ${GA_ID}"
+else
+    echo "Enter a GA tracking ID"
+    read -r GA_ID
+    echo "You entered ${GA_ID}"
+    echo "Is that correct? (y/n)"
+    read -r CONFIRM
+
+    if [ "${CONFIRM}" != "y" ]; then
+        echo "Aborting"
+        exit 1
+    fi
+fi
+
 
 echo "Inserting GTM snippet"
 
-GTM_HEAD_SNIPPET="<\!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l\!='dataLayer'?'\&l='+l:'';j.async=true;j.src='https:\/\/www.googletagmanager.com\/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');<\/script><\!-- End Google Tag Manager -->"
+GTM_HEAD_SNIPPET="<\!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l\!='dataLayer'?'\&l='+l:'';j.async=true;j.src='https:\/\/www.googletagmanager.com\/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');<\/script>"
 
 
-GTM_BODY_SNIPPET="<\!-- Google Tag Manager (noscript) --><noscript><iframe src=\"https:\/\/www.googletagmanager.com\/ns.html?id=${GTM_ID}\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"><\/iframe><\/noscript><\!-- End Google Tag Manager (noscript) -->"
+GTM_BODY_SNIPPET="<\!-- Google Tag Manager (noscript) --><noscript><iframe src=\"https:\/\/www.googletagmanager.com\/ns.html?id=${GTM_ID}\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"><\/iframe><\/noscript>"
 
 GTM_HEAD_PLACEHOLDER='<\!-- INSERT GTM HEAD SNIPPET -->'
 GTM_BODY_PLACEHOLDER='<\!-- INSERT GTM BODY SNIPPET -->'
 
-sed -i".bak" -e "s/${GTM_HEAD_PLACEHOLDER}/${GTM_HEAD_SNIPPET}/" -e"s/${GTM_BODY_PLACEHOLDER}/${GTM_BODY_SNIPPET}/" index.html
+
+GA_SNIPPET="<\!-- Google Analytics --><script async src=\"https:\/\/www.googletagmanager.com\/gtag\/js?id=${GA_ID}\"><\/script> \
+<script> \
+  window.dataLayer = window.dataLayer || []; \
+  function gtag(){dataLayer.push(arguments);} \
+  gtag('js', new Date()); \
+  gtag('config', '${GA_ID}'); \
+<\/script>"
+
+GA_PLACEHOLDER='<!-- INSERT GA SNIPPET -->'
+
+echo "Inserting snippets"
+
+sed -i".bak" -e "s/${GA_PLACEHOLDER}/${GA_SNIPPET}/" -e "s/${GTM_HEAD_PLACEHOLDER}/${GTM_HEAD_SNIPPET}/" -e"s/${GTM_BODY_PLACEHOLDER}/${GTM_BODY_SNIPPET}/" index.html
+
 
 echo "Pushing app"
 
