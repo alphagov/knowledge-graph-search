d62a93dbf2e7d7fa0e739507405386bae5433561
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2023-01-10 15:34:11 +0000

    WIP - taxon queries

--- bigquery.ts
+++ bigquery.ts
@@ -27,28 +27,107 @@ const bigQuery = async function(userQuery: string) {
 
 
 const sendInitQuery: SendInitQuerySignature = async function() {
-  const bqLocales = await bigQuery(`
+  const bqLocalesPromise = bigQuery(`
     SELECT DISTINCT locale
     FROM \`govuk-knowledge-graph.content.locale\`
     ORDER BY locale
   `);
-  const bqTaxons = await bigQuery(`
+  const bqTaxonsPromise = bigQuery(`
     SELECT DISTINCT taxon
     FROM \`govuk-knowledge-graph.content.taxon_ancestors\`
     ORDER BY taxon
   `);
 
+  const bqResults = await Promise.all([bqLocalesPromise, bqTaxonsPromise]);
+
+  // maybe we need to handle exceptions here rather than leave it to the
+  // caller.
+
   const locales = ['', 'en', 'cy'].concat(
-    bqLocales
+    bqResults[0]
       .map((row: any) => row.locale)
       .filter((locale: string) => locale !== 'en' && locale !== 'cy')
   );
+  const taxons = bqResults[1].map((taxon: any) => taxon.taxon_title);
 
-  const result = {
-    taxons: bqTaxons.map((taxon: any) => taxon.taxon_title),
-    locales
-  }
-  return result;
+  return { locales, taxons };
 };
 
+
+/*
+const getTaxonInfo: GetTaxonInfoSignature = async function(name) {
+  const taxonInfo = await bigQuery(`
+    -- This is more annoying than it needs to be, because the
+    -- taxon_levels table uses the homepage_url, not the taxon
+    -- url that we defined, and we couldn't work out how to
+    -- fix it in MongoDB.
+
+    -- Searched-for taxon
+    SELECT
+      taxon.title,
+      has_homepage.homepage_url,
+      taxon_levels.level
+    FROM graph.taxon
+    INNER JOIN graph.has_homepage ON has_homepage.url = taxon.url
+    INNER JOIN content.taxon_levels ON (taxon_levels.url = has_homepage.homepage_url)
+    WHERE taxon.title = 'Land Registration Data'
+    ;
+ `);
+
+  const ancestorsInfo = await bigQuery(`
+    SELECT
+      taxon_ancestors.ancestor_url,
+      taxon_ancestors.ancestor_title,
+      taxon_levels.level
+    FROM graph.taxon_ancestors
+    INNER JOIN graph.has_homepage ON has_homepage.url = taxon_ancestors.ancestor_url
+    INNER JOIN content.taxon_levels ON (taxon_levels.url = has_homepage.homepage_url)
+    WHERE title = 'Land Registration Data'
+    ORDER BY level
+    ;
+  `);
+
+  const childrenInfo = await bigQuery(`
+    SELECT
+      child.title,
+      child_homepage.homepage_url,
+      child_level.level
+    FROM graph.taxon
+    INNER JOIN graph.has_homepage AS parent_homepage USING (url)
+    INNER JOIN graph.taxon_ancestors AS child ON (child.ancestor_title = taxon.title)
+    INNER JOIN graph.has_homepage AS child_homepage ON child_homepage.url = child.url
+    INNER JOIN content.taxon_levels AS parent_level ON (parent_level.url = parent_homepage.homepage_url)
+    INNER JOIN content.taxon_levels AS child_level ON (child_level.url = child_homepage.homepage_url)
+    WHERE taxon.title = 'Land registration'
+    AND child_level.level = parent_level.level + 1
+    ;
+  `);
+
+  const result: Taxon = {
+
+        type: string,
+        name: string,
+        homepage: string,
+        description: string,
+        level:
+        ancestorTaxons: {
+          url: string,
+          name: string,
+          level:
+        }[],
+        childTaxons: {
+          url: string,
+          name: string,
+          level:
+        }[]
+
+  };
+
+return result;
+
+
+};
+*/
+
 export { sendInitQuery };
+//export { sendInitQuery, getTaxonInfo };
