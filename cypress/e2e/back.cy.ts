203164b00fe72b3e02c206ba6f068f4411b4d9dd
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2023-04-06 17:21:58 +0100

    Adapt tests to new tabbed design

--- cypress/e2e/back.cy.ts
+++ cypress/e2e/back.cy.ts
@@ -1,63 +1,13 @@
 describe('Browser buttons behaviour', () => {
-  it('goes back to the previous page when clicking through search types', () => {
-    cy.visit('');
-    cy.contains('#error-summary-title').should('not.exist');
-    cy.contains('button', 'Taxons').click();
-    cy.contains('button', 'Languages').click();
-    cy.go('back')
-    cy.location().should(loc => {
-      expect(loc.search).to.eq('?search-type=taxon');
-    });
-    cy.go('back')
-    cy.location().should(loc => {
-      expect(loc.search).to.eq('');
-    });
-  });
-
   it('goes back when user clicks back after results', () => {
     cy.visit('');
     cy.contains('#error-summary-title').should('not.exist');
-    cy.contains('button', 'Taxons').click();
-    cy.get('input#taxon').type('Air passenger duty');
+    cy.get('input#keyword').type('"Air passenger duty"');
     cy.get('button#search').click();
-    cy.get('#results-heading', { timeout: 60000 }).contains(/^\d+ results$/);
+    cy.get('span[data-target=keyword-results]', { timeout: 60000 }).contains(/\d+ pages/);
     cy.go('back')
     cy.get('ul#show-fields').should('not.exist');
     cy.get('table#results-table').should('not.exist');
-  });
-
-  it('refetches old results when back', () => {
-    cy.visit('');
-    cy.contains('#error-summary-title').should('not.exist');
-    cy.contains('button', 'Taxons').click();
-    cy.get('input#taxon').type('Air passenger duty');
-    cy.get('button#search').click();
-    cy.get('#results-heading', { timeout: 60000 }).contains(/^\d+ results$/);
-    cy.contains('button', 'Languages').click();
-    cy.get('table#results-table').should('not.exist');
-    cy.go('back');
-    cy.get('#results-heading', { timeout: 60000 }).contains(/^\d+ results$/);
-    cy.get('#results-table');
-  });
-
-  it('ends on the start page if going back, forward, back, back', () => {
-    cy.visit('');
-    cy.contains('#error-summary-title').should('not.exist');
-    cy.contains('button', 'Taxons').click();
-    cy.contains('button', 'Languages').click();
-    cy.go('back')
-    cy.go('forward')
-    cy.location().should(loc => {
-      expect(loc.search).to.eq('?search-type=language');
-    });
-    cy.go('back')
-    cy.location().should(loc => {
-      expect(loc.search).to.eq('?search-type=taxon');
-    });
-    cy.go('back')
-    cy.location().should(loc => {
-      expect(loc.search).to.eq('');
-    });
   });
 
   it('shows the start page if the user presses reload on the start page', () => {
@@ -75,13 +25,12 @@ describe('Browser buttons behaviour', () => {
     cy.visit('');
     cy.contains('#error-summary-title').should('not.exist');
     cy.get('#results-heading').should('not.exist');
-    cy.contains('button', 'Taxons').click();
-    cy.get('input#taxon').type('Air passenger duty');
+    cy.get('input#keyword').type('Air passenger duty');
     cy.get('button#search').click();
-    cy.get('#results-heading', { timeout: 60000 }).contains(/^\d+ results$/);
+    cy.get('span[data-target=keyword-results]', { timeout: 60000 }).contains(/\d+ pages/);
     cy.reload();
-    cy.location().should(loc => expect(loc.search).to.eq('?search-type=taxon&selected-taxon=Air+passenger+duty'));
-    cy.get('#results-heading', { timeout: 60000 }).contains(/^\d+ results$/);
+    cy.location().should(loc => expect(loc.search).to.eq('?selected-words=Air+passenger+duty'));
+    cy.get('span[data-target=keyword-results]', { timeout: 60000 }).contains(/\d+ pages/);
     cy.get('#results-table');
   });
 });
