62897aa9bc2b7b4d6ef66fa4e2c6e0f1d0e336f4
somme <sommesak@gmail.com>
2023-06-28 05:41:37 +0100

    added allowGoogleAnalytics middlware

---
+++ src/backend/middleware/allowGoogleAnalytics.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from 'express'
+
+export const allowGoogleAnalytics: RequestHandler = (req, res, next) => {
+  if(req.cookies.acceptAnalytics && process.env.GTM_ID && process.env.GTM_AUTH) {
+    res.locals.allowGoogleAnalytics = true;
+    res.locals.GTM_ID = process.env.GTM_ID;
+    res.locals.GTM_AUTH = process.env.GTM_AUTH;
+  }
+  next();
+};
