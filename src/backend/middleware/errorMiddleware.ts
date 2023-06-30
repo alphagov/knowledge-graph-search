39818628d6cc71b5db19d66ac9e9dc5e5d4ad568
Somme <sommesak@gmail.com>
2023-06-30 12:53:45 +0100

    Update src/backend/middleware/errorMiddleware.ts
    Co-authored-by: Guilhem Forey <guilhem.forey@hotmail.fr>

--- src/backend/middleware/errorMiddleware.ts
+++ src/backend/middleware/errorMiddleware.ts
@@ -5,7 +5,7 @@ export const errorMiddleware = (
   req: Request,
   res: Response
 ): void => {
-  const status = error.response.status || 500
+  const status = error.response?.status || 500
   const title = 'Sorry, there is a problem with the service'
   const message =
     '<p class="govuk-body">Try again later.</p><p class="govuk-body">Please <a class="govuk-link" href="mailto:data-products-research@digital.cabinet-office.gov.uk">contact the Data Products team</a> if the problem persists.</p>'
