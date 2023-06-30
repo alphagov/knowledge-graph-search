a5fed888b066592a3b0c58e746b61c9d035b21af
somme <sommesak@gmail.com>
2023-06-30 11:47:08 +0100

    refactor controller file names

---
+++ src/backend/controllers/search.controller.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express'
+import { ENV } from '../enums/environments'
+import config from '../config'
+
+class SearchController {
+  public search: RequestHandler = (req, res, next) => {
+    const isIntegrationEnv = config.environment === ENV.DEVELOPMENT
+
+    try {
+      res.render('search.njk', {
+        isIntegrationEnv,
+      })
+    } catch (e) {
+      next(e)
+    }
+  }
+}
+
+export default SearchController
