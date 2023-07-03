6e640aabc06fa2ba09457644bc7368b1c62ae566
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-07-03 16:36:50 +0100

    Auth controller unit tests

---
+++ src/backend/controllers/auth.controller.test.ts
@@ -0,0 +1,85 @@
+import request from 'supertest'
+import { expect } from '@jest/globals'
+import e from 'express'
+import App from '../app'
+import { Route } from '../enums/routes'
+import AuthRoutes from '../routes/AuthRoutes'
+import AuthController from './auth.controller'
+import * as redisStore from '../services/redisStore'
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+afterAll(async () => {
+  await new Promise<void>((resolve) => setTimeout(() => resolve(), 500))
+})
+
+describe('Auth Controller', () => {
+  describe('reauth', () => {
+    it('Uses the user ID from query parameters', async () => {
+      const mockRequest = {} as e.Request
+      mockRequest.params = {
+        userId: 'testId',
+      }
+      const mockResponse = {} as e.Response
+      mockResponse.send = jest.fn()
+      const mockNext = {} as e.NextFunction
+      jest
+        .spyOn(redisStore, 'destroySessionsForUserId')
+        .mockImplementation(jest.fn())
+
+      const controller = new AuthController()
+      await controller.reauth(mockRequest, mockResponse, mockNext)
+      expect(redisStore.destroySessionsForUserId).toHaveBeenCalledWith('testId')
+    })
+    it('Returns 200 with message if reauth successful', async () => {
+      const mockRequest = {} as e.Request
+      mockRequest.params = {
+        userId: 'testId',
+      }
+      const mockResponse = {} as e.Response
+      mockResponse.send = jest.fn()
+      const mockNext = {} as e.NextFunction
+      jest
+        .spyOn(redisStore, 'destroySessionsForUserId')
+        .mockImplementation(jest.fn())
+
+      const controller = new AuthController()
+      await controller.reauth(mockRequest, mockResponse, mockNext)
+      expect(mockResponse.send).toHaveBeenCalledWith(
+        'User logged out of GovSearch successfully. UserId = testId'
+      )
+    })
+    it('Returns 200 witn no message even if ther is an error', async () => {
+      const mockRequest = {} as e.Request
+      mockRequest.params = {
+        userId: 'testId',
+      }
+      const mockResponse = {} as e.Response
+      mockResponse.status = jest.fn()
+      const mockNext = {} as e.NextFunction
+      jest
+        .spyOn(redisStore, 'destroySessionsForUserId')
+        .mockRejectedValue(new Error('Test error'))
+
+      const controller = new AuthController()
+      await controller.reauth(mockRequest, mockResponse, mockNext)
+      expect(mockResponse.status).toHaveBeenCalledWith(200)
+    })
+  })
+
+  describe('On login with success', () => {
+    it('Redirects to the search endpoint', async () => {
+      const mockRequest = {} as e.Request
+      const mockResponse = {} as e.Response
+      const mockNext = {} as e.NextFunction
+      mockResponse.redirect = jest.fn()
+
+      const controller = new AuthController()
+      await controller.loginSuccessRedirect(mockRequest, mockResponse, mockNext)
+
+      expect(mockResponse.redirect).toHaveBeenCalledWith(Route.search)
+    })
+  })
+})
