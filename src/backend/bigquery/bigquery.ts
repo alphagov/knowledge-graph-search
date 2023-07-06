ab09542721736ade2dfb9c076012f463bbd0572a
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-07-06 20:20:23 +0100

    Added buildQuery tests

--- src/backend/bigquery/bigquery.ts
+++ src/backend/bigquery/bigquery.ts
@@ -257,7 +257,7 @@ const buildSqlQuery = function (
 
   const includeClause =
     keywords.length === 0
-      ? ''
+      ? null
       : 'AND (' +
         [...Array(keywords.length).keys()]
           .map((index) =>
@@ -270,7 +270,7 @@ const buildSqlQuery = function (
 
   const excludeClause =
     excludedKeywords.length === 0
-      ? ''
+      ? null
       : 'AND NOT (' +
         [...Array(excludedKeywords.length).keys()]
           .map((index) =>
@@ -280,83 +280,97 @@ const buildSqlQuery = function (
           )
           .join(' OR ') +
         ')'
-  let areaClause = ''
+
+  let areaClause = null
   if (searchParams.areaToSearch === 'publisher') {
     areaClause = 'AND publishing_app = "publisher"'
   } else if (searchParams.areaToSearch === 'whitehall') {
     areaClause = 'AND publishing_app = "whitehall"'
   }
 
-  let localeClause = ''
+  let localeClause = null
   if (searchParams.selectedLocale !== '') {
     localeClause = `AND locale = @locale`
   }
 
-  let taxonClause = ''
+  let taxonClause = null
   if (searchParams.selectedTaxon !== '') {
     taxonClause = `
-      AND EXISTS
-        (
-          SELECT 1 FROM UNNEST (taxons) AS taxon
-          WHERE taxon = @taxon
-        )
-    `
+AND EXISTS
+  (
+    SELECT 1 FROM UNNEST (taxons) AS taxon
+    WHERE taxon = @taxon
+  )
+`
   }
 
-  let organisationClause = ''
+  let organisationClause = null
   if (searchParams.selectedOrganisation !== '') {
     organisationClause = `
-      AND EXISTS
-        (
-          SELECT 1 FROM UNNEST (organisations) AS link
-          WHERE link = @organisation
-        )
-    `
+AND EXISTS
+  (
+    SELECT 1 FROM UNNEST (organisations) AS link
+    WHERE link = @organisation
+  )
+`
   }
 
-  let linkClause = ''
+  let linkClause = null
   if (searchParams.linkSearchUrl !== '') {
     // Link search: look for url as substring
     linkClause = `
-      AND EXISTS
-        (
-          SELECT 1 FROM UNNEST (hyperlinks) AS link
-          WHERE CONTAINS_SUBSTR(link, @link)
-        )
-    `
+AND EXISTS
+  (
+    SELECT 1 FROM UNNEST (hyperlinks) AS link
+    WHERE CONTAINS_SUBSTR(link, @link)
+  )
+`
   }
 
-  return `
-    SELECT
-      url,
-      title,
-      documentType,
-      contentId,
-      locale,
-      publishing_app,
-      first_published_at,
-      public_updated_at,
-      withdrawn_at,
-      withdrawn_explanation,
-      page_views,
-      taxons,
-      primary_organisation,
-      organisations AS all_organisations
-    FROM search.page
+  const prefix = `
+SELECT
+  url,
+  title,
+  documentType,
+  contentId,
+  locale,
+  publishing_app,
+  first_published_at,
+  public_updated_at,
+  withdrawn_at,
+  withdrawn_explanation,
+  page_views,
+  taxons,
+  primary_organisation,
+  organisations AS all_organisations
+FROM search.page
 
-    WHERE TRUE
-    ${includeClause}
-    ${excludeClause}
-    ${areaClause}
-    ${localeClause}
-    ${taxonClause}
-    ${organisationClause}
-    ${linkClause}
+WHERE TRUE`
 
-    ORDER BY page_views DESC
+  const suffix = `
+ORDER BY page_views DESC
+LIMIT 10000`
 
-    LIMIT 10000
-  `
+  const clauses = [
+    includeClause,
+    excludeClause,
+    areaClause,
+    localeClause,
+    taxonClause,
+    organisationClause,
+    linkClause,
+  ]
+
+  let clausesPart = ''
+  clauses.forEach((clause) => {
+    if (clause) {
+      clausesPart += `\n${clause}`
+    }
+  })
+
+  const finalQuery = `${prefix}${clausesPart}${suffix}`
+
+  return finalQuery
 }
 
 export {
