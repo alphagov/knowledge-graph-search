9d4310686d32dcfd717797472f34da5465452539
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-07-07 10:03:00 +0100

    Add SessionStore tests

--- src/backend/services/sessionStore.test.ts
+++ src/backend/services/sessionStore.test.ts
@@ -1,26 +1,62 @@
+import { expect } from '@jest/globals'
 import { SessionStore } from './sessionStore'
 
+jest.mock('ioredis', () => {
+  class RedisMock {
+    private port: number
+    private host: string
+    constructor(port: number, host: string) {
+      this.port = port
+      this.host = host
+    }
+
+    on() {
+      return 'void'
+    }
+  }
+  return {
+    __esModule: true,
+    default: RedisMock,
+  }
+})
+
+let sadd: jest.Mock
+let spop: jest.Mock
+let destroy: jest.Mock
+
 beforeAll(() => {
+  sadd = jest.fn()
+  spop = jest.fn()
+  destroy = jest.fn()
+  jest.clearAllMocks()
+  jest.resetAllMocks()
   ;(
     jest.spyOn(
-      SessionStore.prototype,
+      SessionStore.prototype as any,
       'createRedisInstance'
-    ) as jest.Mock<SessionStore>
-  ).mockImplementation(() => 'A redis instance')
+    ) as jest.Mock
+  ).mockImplementation(() => ({ sadd, spop }))
   ;(
-    jest.spyOn(
-      SessionStore.prototype,
-      'createSessionStore'
-    ) as jest.Mock<SessionStore>
-  ).mockImplementation(() => 'A session store')
+    jest.spyOn(SessionStore.prototype as any, 'createSessionStore') as jest.Mock
+  ).mockImplementation(() => ({ destroy }))
 })
 
-afterAll(() => {
-  jest.restoreAllMocks()
-})
+describe('[Class]: SessionStore]', () => {
+  it('destroySession calls destroy on sessionStore', async () => {
+    const sessionStore = new SessionStore()
+    await sessionStore.destroySession('testId')
+    expect(sessionStore.getStore().destroy).toHaveBeenCalledWith('testId')
+  })
 
-test('Modify class', () => {
-  const newSessionStore = new SessionStore()
-
-  expect(newSessionStore.getStore()).equal('A session store')
+  it('destroySessionsForUserId', async () => {
+    const sessionStore = new SessionStore()
+    spop.mockResolvedValueOnce('id1').mockResolvedValueOnce('id2')
+    await sessionStore.destroySessionsForUserId('userTestId')
+    expect(spop).toHaveBeenNthCalledWith(1, 'UserSessionsSet__userTestId')
+    expect(spop).toHaveBeenNthCalledWith(2, 'UserSessionsSet__userTestId')
+    expect(spop).toHaveBeenCalledTimes(3)
+    expect(destroy).toHaveBeenCalledWith('id1')
+    expect(destroy).toHaveBeenCalledWith('id2')
+    expect(destroy).toHaveBeenCalledTimes(3)
+  })
 })
