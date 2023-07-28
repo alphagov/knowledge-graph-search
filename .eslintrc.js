baa48e34e14a2c6fb8a2cc2386e5bf1df9e3d452
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-07-28 16:33:01 +0100

    Create production pipeline (#121) (#122)
    * Create production pipeline (#121)
    
    * Add CD for Production
    
    * Disable code hygiene temporarily
    
    * Action for creating release tags
    
    * Reenable ci checks (#123)
    
    * Re-enable CI checks
    
    * Rename workflows with prefix grouping
    
    * Dedicated tsconfig for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
   },
   plugins: ['@typescript-eslint', 'import', 'cypress', 'jest'],
   rules: {
