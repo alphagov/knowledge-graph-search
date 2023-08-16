a6b6cae68a383dfcf970d79c22a80d9aaa44ab33
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-08-16 11:23:26 +0100

    Change results per page options

--- src/frontend/config.ts
+++ src/frontend/config.ts
@@ -11,7 +11,7 @@ interface FrontendConfig {
 const config: FrontendConfig = {
   pagination: {
     defaultResultsPerPage: 20,
-    options: [10, 20, 30, 50, 100],
+    options: [10, 20, 50, 100, 200],
     maxResultsBeforeScrolling: 20,
   },
 }
