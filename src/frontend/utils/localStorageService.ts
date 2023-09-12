57dae21a71f2ceb1023baa09cc10283126e470ed
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-09-12 19:55:07 +0100

    t: save layout in localstorage state

--- src/frontend/utils/localStorageService.ts
+++ src/frontend/utils/localStorageService.ts
@@ -1,10 +1,11 @@
 const SHOWFIELDS_KEY = 'state.showFields'
+const LAYOUT_KEY = 'state.panels'
 
 export const saveShowFieldsState = (showFields: any) => {
   try {
     localStorage.setItem(SHOWFIELDS_KEY, JSON.stringify(showFields))
   } catch (error) {
-    console.error('Failed to save state to localStorage:', error)
+    console.error('Failed to save layout state to localStorage:', error)
   }
 }
 
@@ -13,7 +14,27 @@ export const loadShowFieldsState = () => {
     const data = localStorage.getItem(SHOWFIELDS_KEY)
     return data ? JSON.parse(data) : null
   } catch (error) {
-    console.error('Failed to load state from localStorage:', error)
+    console.error('Failed to load layout state from localStorage:', error)
+    return null
+  }
+}
+
+type LayoutState = { showFiltersPane: boolean; showFieldSet: boolean }
+
+export const saveLayoutState = (layoutState: LayoutState) => {
+  try {
+    localStorage.setItem(LAYOUT_KEY, JSON.stringify(layoutState))
+  } catch (error) {
+    console.error('Failed to save layout state to localStorage:', error)
+  }
+}
+
+export const loadLayoutState = () => {
+  try {
+    const data = localStorage.getItem(LAYOUT_KEY)
+    return data ? JSON.parse(data) : null
+  } catch (error) {
+    console.error('Failed to load layout state from localStorage:', error)
     return null
   }
 }
