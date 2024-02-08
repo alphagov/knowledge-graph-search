74c98e0ff22e34e42828d085cbd92a1451e2475c
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-02-08 14:55:36 +0000

    Remove the infobox
    It has been disabled for a long time via a feature flag. It is now
    merely a maintenance burden.

--- src/backend/routes/IndexRoutes.ts
+++ src/backend/routes/IndexRoutes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import MeController from '../controllers/me.controller'
 import SearchController from '../controllers/search.controller'
 import SearchAPIController from '../controllers/searchAPI.controller'
-import InfoBoxController from '../controllers/infoBox.controller'
 import DownloadCSVController from '../controllers/downloadCSV.controller'
 import CookiesController from '../controllers/cookies.controller'
 import FeedbackSurveyController from '../controllers/feedbackSurvey.controller'
@@ -14,7 +13,6 @@ class IndexRoute implements Routes {
   public meController = new MeController()
   public searchController = new SearchController()
   public searchAPIController = new SearchAPIController()
-  public infoBoxController = new InfoBoxController()
   public downloadCSVController = new DownloadCSVController()
   public cookiesController = new CookiesController()
   public feedbackSurveyController = new FeedbackSurveyController()
@@ -37,45 +35,9 @@ class IndexRoute implements Routes {
     this.router.get(Route.searchApi, auth(), this.searchAPIController.searchApi)
 
     this.router.get(
-      Route.searchTaxon,
-      auth(),
-      this.searchAPIController.searchTaxon
-    )
-
-    this.router.get(
       Route.downloadCSV,
       auth(),
       this.downloadCSVController.downloadCSV
-    )
-
-    this.router.get(
-      Route.infoBoxOrganisation,
-      auth(),
-      this.infoBoxController.infoBoxOrganisation
-    )
-
-    this.router.get(
-      Route.infoBoxRole,
-      auth(),
-      this.infoBoxController.infoBoxRole
-    )
-
-    this.router.get(
-      Route.infoBoxBankHoliday,
-      auth(),
-      this.infoBoxController.infoBoxBankHoliday
-    )
-
-    this.router.get(
-      Route.infoBoxTransaction,
-      auth(),
-      this.infoBoxController.infoBoxTransaction
-    )
-
-    this.router.get(
-      Route.infoBoxPerson,
-      auth(),
-      this.infoBoxController.infoBoxPerson
     )
 
     this.router.get(Route.cookies, auth(), this.cookiesController.cookies)
--- src/frontend/search-api.ts
+++ src/frontend/search-api.ts
@@ -98,84 +98,23 @@ const queryBackend: (
   try {
     apiResults = await fetchWithTimeout(url, 300)
   } catch (error: any) {
-    console.log('error running main+meta queries', error)
+    console.log('error running queries', error)
     // TODO: find another way than using a callback function to get rid of the eslint error
     // eslint-disable-next-line n/no-callback-literal
     callback({ type: EventType.SearchApiCallbackFail, error })
     return
   }
-  let { main, meta } = apiResults
-  // If there's an exact match within the meta results, just keep that one
-  const searchKeywords: string = searchParams.selectedWords.replace(/"/g, '')
-  const exactMetaResults = meta.filter((result: any) => {
-    return result.name.toLowerCase() === searchKeywords.toLowerCase()
+  let { main } = apiResults
+
+  // TODO: find another way than using a callback function to get rid of the eslint error
+  // eslint-disable-next-line n/no-callback-literal
+  callback({
+    type: EventType.SearchApiCallbackOk,
+    results: { main },
   })
-  if (exactMetaResults.length === 1) {
-    meta = exactMetaResults
-  }
-  if (meta.length === 1) {
-    // one meta result: show the knowledge panel (may require more API queries)
-    const fullMetaResults = await buildMetaboxInfo(meta[0])
-    // TODO: find another way than using a callback function to get rid of the eslint error
-    // eslint-disable-next-line n/no-callback-literal
-    callback({
-      type: EventType.SearchApiCallbackOk,
-      results: { main, meta: fullMetaResults },
-    })
-    // } else if (metaResults.length >= 1) {
-    //   // multiple meta results: we'll show a disambiguation page
-    //   callback({ type: EventType.SearchApiCallbackOk, results: { main, meta: metaResults } });
-  } else {
-    // no meta results
-    // TODO: find another way than using a callback function to get rid of the eslint error
-    // eslint-disable-next-line n/no-callback-literal
-    callback({
-      type: EventType.SearchApiCallbackOk,
-      results: { main, meta: null },
-    })
-  }
 }
 
 //= ========== private ===========
-
-const buildMetaboxInfo = async function (info: any) {
-  console.log(`Found a ${info.type}. Running extra queries`)
-  console.log(info)
-  switch (info.type) {
-    case 'BankHoliday': {
-      return await fetchWithTimeout(
-        `/bank-holiday?name=${encodeURIComponent(info.name)}`
-      )
-    }
-    case 'Person': {
-      return await fetchWithTimeout(
-        `/person?name=${encodeURIComponent(info.name)}`
-      )
-    }
-    case 'Role': {
-      return await fetchWithTimeout(
-        `/role?name=${encodeURIComponent(info.name)}`
-      )
-    }
-    case 'Organisation': {
-      return await fetchWithTimeout(
-        `/organisation?name=${encodeURIComponent(info.name)}`
-      )
-    }
-    case 'Transaction': {
-      return await fetchWithTimeout(
-        `/transaction?name=${encodeURIComponent(info.name)}`
-      )
-    }
-    case 'Taxon': {
-      return await fetchWithTimeout(
-        `/taxon?name=${encodeURIComponent(info.name)}`
-      )
-    }
-    default:
-      console.log('unknown meta node type', info.type)
-  }
-}
 
 export {
   makeURLfromSearchParams as makeQueryString,
