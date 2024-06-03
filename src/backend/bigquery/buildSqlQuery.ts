5ec3cb07b1231234d7b9ad4934a272e625349ce7
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-06-03 10:25:34 +0100

    feat(backend): add the `linksExactMatch` boolean param to sql query

--- src/backend/bigquery/buildSqlQuery.ts
+++ src/backend/bigquery/buildSqlQuery.ts
@@ -150,14 +150,29 @@ export const buildSqlQuery = function (
   let linkClause = ''
   if (searchParams.linkSearchUrl !== '') {
     // Link search: look for url as substring
-    linkClause = `
-      AND EXISTS
-        (
-          SELECT 1 FROM UNNEST (hyperlinks) AS link
-          WHERE CONTAINS_SUBSTR(link.link_url, @link)
-        )
-    `
-  }
+    if (searchParams.linksExactMatch) {
+      linkClause = `
+        AND EXISTS
+          (
+            SELECT 1 FROM UNNEST (hyperlinks) AS link
+            WHERE
+            @link = link.link_url
+            OR @link = CONCAT('http://www.gov.uk', link.link_url)
+            OR @link = CONCAT('http://www.gov.uk/', link.link_url)
+            OR @link = CONCAT('https://www.gov.uk', link.link_url)
+            OR @link = CONCAT('https://www.gov.uk/', link.link_url)
+          )
+      `
+    }
+    else {
+      linkClause = `
+        AND EXISTS
+          (
+            SELECT 1 FROM UNNEST (hyperlinks) AS link
+            WHERE CONTAINS_SUBSTR(link.link_url, @link)
+          )
+      `
+    }
 
   let phoneNumberClause = ''
   if (searchParams.phoneNumber && searchParams.phoneNumber !== '') {
