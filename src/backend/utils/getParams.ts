3a31908fe178ef5f3d5d5c03bfc96fcd1dca5d25
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-06-12 14:01:00 +0100

    Don't sanitise form input
    * SQL injection is mitigated by using BigQuery parameters.
    * Any changes to the values of list-items break filtering, such as when
      trimming trailing whitespace from the title of https://www.gov.uk/api/content/government/organisations/department-for-levelling-up-housing-and-communities, which means that none of its publications appear in search results when filtering for that publishing organisation.

--- src/backend/utils/getParams.ts
+++ src/backend/utils/getParams.ts
@@ -9,51 +9,50 @@ import {
   PublishingStatus,
   PoliticalStatus,
 } from '../../common/types/search-api-types'
-import { sanitiseInput } from '../../common/utils/utils'
 
 export const getParams = (req: express.Request): SearchParams => {
   const searchType = <SearchType>(
-    (sanitiseInput(req.query[UrlParams.SearchType] as string) ||
+    ((req.query[UrlParams.SearchType] as string) ||
       SearchType.Keyword)
   )
   const selectedWords =
-    sanitiseInput(req.query[UrlParams.SelectedWords] as string) || ''
+    (req.query[UrlParams.SelectedWords] as string) || ''
   const excludedWords =
-    sanitiseInput(req.query[UrlParams.ExcludedWords] as string) || ''
-  const taxon = sanitiseInput(req.query[UrlParams.Taxon] as string) || ''
+    (req.query[UrlParams.ExcludedWords] as string) || ''
+  const taxon = (req.query[UrlParams.Taxon] as string) || ''
   const publishingOrganisation =
-    sanitiseInput(req.query[UrlParams.PublishingOrganisation] as string) || ''
-  const language = sanitiseInput(req.query[UrlParams.Language] as string) || ''
+    (req.query[UrlParams.PublishingOrganisation] as string) || ''
+  const language = (req.query[UrlParams.Language] as string) || ''
   const caseSensitive = req.query[UrlParams.CaseSensitive] === 'true'
   const combinator = <Combinator>(
-    (sanitiseInput(req.query[UrlParams.Combinator] as string) || Combinator.All)
+    ((req.query[UrlParams.Combinator] as string) || Combinator.All)
   )
   const documentType =
-    sanitiseInput(req.query[UrlParams.DocumentType] as string) || ''
+    (req.query[UrlParams.DocumentType] as string) || ''
 
   const keywordLocation =
-    (sanitiseInput(
+    ((
       req.query[UrlParams.KeywordLocation] as string
     ) as KeywordLocation) || KeywordLocation.All
 
   const publishingApplication = <PublishingApplication>(
-    (sanitiseInput(req.query[UrlParams.PublishingApplication] as string) ||
+    ((req.query[UrlParams.PublishingApplication] as string) ||
       PublishingApplication.Any)
   )
   const linkSearchUrl =
-    sanitiseInput(req.query[UrlParams.LinkSearchUrl] as string) || ''
+    (req.query[UrlParams.LinkSearchUrl] as string) || ''
   const phoneNumber =
-    sanitiseInput(req.query[UrlParams.PhoneNumber] as string) || ''
+    (req.query[UrlParams.PhoneNumber] as string) || ''
 
-  const publishingStatus = sanitiseInput(
+  const publishingStatus = (
     req.query[UrlParams.PublishingStatus] as string
   ) as PublishingStatus
   const politicalStatus = <PoliticalStatus>(
-    (sanitiseInput(req.query[UrlParams.PoliticalStatus] as string) ||
+    ((req.query[UrlParams.PoliticalStatus] as string) ||
       PoliticalStatus.Any)
   )
   const government =
-    sanitiseInput(req.query[UrlParams.Government] as string) || ''
+    (req.query[UrlParams.Government] as string) || ''
 
   return {
     searchType,
