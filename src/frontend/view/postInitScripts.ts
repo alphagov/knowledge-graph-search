65d8a367a3fe06ac592f269561a4170d7cf9132a
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-09-02 14:44:41 +0100

    fix(fe): associated persons search query and side filter placeholders

--- src/frontend/view/postInitScripts.ts
+++ src/frontend/view/postInitScripts.ts
@@ -25,6 +25,9 @@ export default function govukPostInitScripts() {
       'side-filters-taxon': 'a topic tag',
       'search-filters-taxon': 'a topic tag',
       'search-filters-person': 'a person',
+      'side-filters-person': 'a person',
+      'side-filters-government': 'a government',
+      'search-filters-government': 'a government',
     }
 
     var getPlaceholder = (id) => 'Start typing ' + placeholderMapping[id] || "a " + id
